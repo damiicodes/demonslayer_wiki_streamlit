564b1946299305d0fc346b8ff638145d43d38e22
Dami <dami.fajinmi@hotmail.com>
2023-04-15 12:11:38 +0100

    Added images and descriptions to page

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import streamlit as st
 import pandas
 
+st.set_page_config(layout='wide')
+
+df = pandas.read_csv('demonslayers.csv', sep=',')
+
+
 st.title('Demon Slayer Wiki')
 
 col1, col2 = st.columns([1, 3])
 col3 = st.container()
 col4 = st.container()
+col5, empty_col, col6 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image('images/logo.png')
@@ -23,3 +29,17 @@ with col3:
 with col4:
     st.write('<p> Below is a list of characters that appear in the Demon Slayer Anime </p>',
              unsafe_allow_html=True)
+
+with col5:
+    for index, row in df[:6].iterrows():
+        st.header(row['character name'])
+        st.write(row['description'])
+        st.image('images/' + row['image'], width=400)
+        st.write('Episode Appearances:' + " " + row['episode appearance'])
+
+with col6:
+    for index, row in df[6:].iterrows():
+        st.header(row['character name'])
+        st.write(row['description'])
+        st.image('images/' + row['image'], width=400)
+        st.write('Episode Appearances:' + " " + row['episode appearance'])
